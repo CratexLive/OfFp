ad6a652b1712c5bded17f27205ba3f21b9eed8a5
CratexLive <drivexluxe@gmail.com>
2026-09-16 19:57:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ export const CONFIG = {
   appName: "CricxCrate",
   jsonUrl: "https://raw.githubusercontent.com/drmlive/fancode-live-events/refs/heads/main/fancode.json",
   telegramUrl: "https://t.me/+m4odTq36LL4xNmI1",
-  cacheKey: "_cricxcrate_shadow_v1"
+  cacheKey: "_cricxcrate_flat_v1"
 };
